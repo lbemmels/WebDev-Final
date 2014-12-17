2ae83c82c7b39819cf30e9007c8908c945e5c322
Lindsey Bemmels <lbemmels@mcad.edu>
2014-12-17 13:56:21 -0600

    Mid-week
    Fixed the jumping Nav
    Revised the Homepage
    Tried a view layout adjustments

--- js/main.js
+++ js/main.js
@@ -1,1 +1,59 @@
+var imageCount = 1;
+var total = 3;
+
+function fox(x) {
+	var image = document.getElementById('imgslide');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "../../img/fox" + imageCount + ".jpg";
+}
+
+function p1(x) {
+	var image = document.getElementById('imgslide');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "../../img/p1" + imageCount + ".jpg";
+}
+
+function social(x) {
+	var image = document.getElementById('imgslide');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "../../img/social" + imageCount + ".jpg";
+}
+
+function define(x) {
+	var image = document.getElementById('imgslide');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "../../img/define" + imageCount + ".jpg";
+}
+
+function ddw(x) {
+	var image = document.getElementById('imgslide');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "../../img/ddw" + imageCount + ".jpg";
+}
+
+function state(x) {
+	var image = document.getElementById('imgslide');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "../../img/state" + imageCount + ".jpg";
+}
+
+function vinyl(x) {
+	var image = document.getElementById('imgslide');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "../../img/vinyl" + imageCount + ".jpg";
+}
 
